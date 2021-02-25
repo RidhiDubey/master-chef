01b81e8114bff7e2581659da37ea3fe0fd9f46d1
RidhiDubey <76939922+RidhiDubey@users.noreply.github.com>
2021-02-25 11:56:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+text("Recipe of making grilled sandwich");
+text("First take two slices of bread and chopped vegetables.");
+text("spread butter on the slices.");
+text("keep the chopped vegetables between the brad slices.");
+text("grese the butter with oil.");
+text("place the bread sandwich in the toaster.");
+text("switch on the toaster and grill the sandwich.");
+text("When the sandwich is ready, serve it hot");
